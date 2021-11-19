d52856c900a1915e6a9519a85fa8f2146f2cbcc3
Elia Gamberi <gamberoatomico@gmail.com>
2021-11-19 22:53:22 +0100

    #1 :lipstick: first home

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,8 +2,6 @@ $(document).on('turbolinks:load', () => {
     let body_data = $('body').data();
     /*global functions */
 
-    console.log(body_data.controller + '#' + body_data.action + ':loaded')
-
     // Eventi
     $(window).trigger(body_data.controller + ':loaded')
     $(window).trigger(body_data.controller + '#' + body_data.action + ':loaded')
--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,2 +1,21 @@
 $(window).on('home#main:loaded', () => {
+    let animation = anime({
+        targets: '.delibird',
+        translateX: -100,
+        duration: 2000
+    })
+
+    $('.delibird').on("click", () => {movePresent()})
+
+    function movePresent(){
+        let animation = anime({
+            targets: '.present',
+            translateX: 1000,
+            rotate: '1turn',
+            duration: 3000,
+        })
+        setTimeout(function(){ window.location.href = $('.delibird').data('newpresent'); }, 1000)
+    }
+
+
 })
