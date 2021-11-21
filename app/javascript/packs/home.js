bb6da6da9448a7283846fe5d8b6644f53c50c94f
Elia Gamberi <gamberoatomico@gmail.com>
2021-11-21 06:10:55 +0100

    #2 :lipstick: added font and styled battle

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,16 +1,26 @@
 $(window).on('home#main:loaded', () => {
     let animation = anime({
         targets: '.delibird',
-        translateX: -100,
-        duration: 2000
+        translateX: -20,
+        rotate: function() {
+            return [5, -10, 5]
+        },
+        easing: function(el, i, total) {
+            return function(t) {
+                return Math.pow(Math.sin(t * (i + 0.5)), total);
+            }
+        },
+        direction: 'alternate',
+        duration: 1000,
+        loop: true
     })
 
-    $('.delibird').on("click", () => {movePresent()})
+    $('.fight').on("click", () => {movePresent()})
 
     function movePresent(){
         let animation = anime({
             targets: '.present',
-            translateX: 1000,
+            translateX: [380, 1000],
             rotate: '1turn',
             duration: 3000,
         })
