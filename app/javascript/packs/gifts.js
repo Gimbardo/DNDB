9ac7dde2494f9b6ffe58317249dcdef9e75d8e7d
Elia Gamberi <gamberoatomico@gmail.com>
2021-11-26 18:15:26 +0100

    #3 #4 #5 :lipstick: added back button and minor fixes

--- app/javascript/packs/gifts.js
+++ app/javascript/packs/gifts.js
@@ -2,10 +2,10 @@ $(window).on('gifts#new:loaded', () => {
     console.log("aaaa")
     anime({
         targets: ".gift",
-        translateY: -30,
+        translateY: [15, -15],
         loop: true,
         direction: 'alternate',
-        duration: 2000,
+        duration: 1800,
         easing: 'easeInOutSine'
     })
 });
