ac6ce64cdce70089d78fd1d222ec25d92af76ce4
Elia Gamberi <elia.gamberi@widegroup.eu>
2024-01-24 01:24:55 +0100

    oh fuck sono su masyer, vabbè è troppo tardi

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
 
+bundle exec rails db:prepare
+
 ./bin/webpack
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
